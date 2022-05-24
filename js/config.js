c463031e990086c520c95c557bf94f3c8434f235
morihisashi <hisashi.mori@humanage.co.jp>
2022-05-24 10:05:34 +0900

    保存機能の追加

--- js/config.js
+++ js/config.js
@@ -4,7 +4,11 @@ function changeBackColor() {
   let r = document.getElementById('bgColorR').value;
   let g = document.getElementById('bgColorG').value;
   let b = document.getElementById('bgColorB').value;
-    
+  
+  //ローカルストレージへの登録
+  localStorage.setItem('main-bg-color_R', r);
+  localStorage.setItem('main-bg-color_G', g);
+  localStorage.setItem('main-bg-color_B', b);
   //CSSを変更
   let rgb = "rgb(" + r + "," + g + "," + b + ")";
   document.documentElement.style.setProperty('--main-bg-color', rgb);
@@ -12,6 +16,11 @@ function changeBackColor() {
 
 //設定色を初期状態にリセットする関数
 function resetBackColor() {
+  //ローカルストレージのリセット
+  localStorage.removeItem('main-bg-color_R');
+  localStorage.removeItem('main-bg-color_G');
+  localStorage.removeItem('main-bg-color_B');
+
   //画面値を初期値に
   document.getElementById('bgColorR').value = parseInt('ff', 16);
   document.getElementById('bgColorG').value = parseInt('57', 16);
@@ -40,4 +49,23 @@ function resetFontColor() {
   document.getElementById('fontColorB').value = parseInt('70', 16);
 
   changeFontColor();
+}
+
+function drtBackColor() {
+  //ローカルストレージ内のデータ有無確認
+  if(localStorage.getItem('main-bg-color_B') == null){
+    resetBackColor();
+  } else {
+    //ローカルストレージデータの取得
+    let r = localStorage.removeItem('main-bg-color_R');
+    let g = localStorage.removeItem('main-bg-color_G');
+    let b = localStorage.removeItem('main-bg-color_B');
+    //画面値に反映
+    document.getElementById("bgColorR").value = r;
+    document.getElementById("bgColorG").value = g;
+    document.getElementById("bgColorB").value = b;
+
+    changeBackColor();
+  }
+  
 }
